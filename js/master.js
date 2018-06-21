772868164dae5f5179d7adc965b6d1b3c664822b
Julian Marmier <julianmarmier@gmail.com>
2018-06-21 16:52:14 -0400

    Updated template

--- js/master.js
+++ js/master.js
@@ -2,8 +2,29 @@
 $(document).ready(function () {
   var bannerClose = document.getElementById('close');
   var banner = document.getElementById('top-banner');
+  var bannerLink = document.getElementById('toplink');
+
+  bannerClose.onmouseover = function () {
+    banner.classList.toggle("white");
+    bannerClose.style.color = "black";
+    bannerLink.style.color = "#6abab5";
+  }
+
+  bannerClose.onmouseout = function () {
+    banner.classList.toggle("white");
+    bannerClose.style.color = "white";
+    bannerLink.style.color = "#ffeb3b";
+  }
 
   bannerClose.onclick = function () {
     banner.classList.add("slide-up");
+    //banner.style.display = "none";
+  }
+
+  bannerLink.onmouseover = function () {
+    bannerLink.style.color = "white";
+  }
+  bannerLink.onmouseout = function () {
+    bannerLink.style.color = "#ffeb3b"
   }
 })
