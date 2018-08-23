590e31aa180adcafd862a9a849355a3bc8dd7feb
Julian Marmier <julianmarmier@gmail.com>
2018-08-23 10:35:15 -0400

    Added Lightbox functionality to the website and improved flex

--- js/master.js
+++ js/master.js
@@ -12,6 +12,11 @@ function returnHome() {
   window.location.href = "index.html"
 }
 
+function moveArrow() {
+  console.log("hover");
+  $('#ig-arrow').classList.toggle("moveRight");
+}
+
 $(function(){
     var includes = $('[data-include]');
     jQuery.each(includes, function(){
@@ -53,4 +58,70 @@ $(document).ready(function () {
   bannerLink.onmouseout = function () {
     bannerLink.style.color = "#ffeb3b"
   }
+
+  $(function () {
+    for (var i = 1; i < 18; i++) {
+      var caption;
+      switch (i) {
+        case 1:
+          caption = 'A mouse in the woods. June 2016. <a href="http://instagram.com/abhinav.mukherjee59714">@abhinav.mukherjee59714</a>'
+          break;
+        case 2:
+          caption = '🌸flowers in front of temple by Catherine Sun <a href="http://instagram.com/catherinesun7091">@catherinesun7091</a>'
+          break;
+        case 3:
+          caption = 'By <a href="http://instagram.com/julianmarmier">@julianmarmier</a>'
+          break;
+        case 4:
+          caption = 'A droplet 💧 on a flower by <a href="http://instagram.com/abhinav.mukherjee59714">@abhinav.mukherjee59714</a>'
+          break;
+        case 5:
+          caption = 'Temple of Poseidon<br>Photography by: Katie Chamberlain <a href="http://instagram.com/katiebc_">@katiebc_</a>'
+          break;
+        case 6:
+          caption = "Photographer: Grégory Murphy"
+          break;
+        case 7:
+          caption = "Photographer: Vibhuti Pathare"
+          break;
+        case 8:
+          caption = "Photographer: Divya Jain"
+          break;
+        case 9:
+          caption = "Photographer: Katie Chamberlain"
+          break;
+        case 10:
+          caption = "Photographer: Sean Feldman"
+          break;
+        case 11:
+          caption = "Photographer: Niha Putta"
+          break;
+        case 12:
+          caption = "Photographer: Jenny L"
+          break;
+        case 13:
+          caption = "Photographer: Vibhuti Pathare"
+          break;
+        case 14:
+          caption = "Photographer: Kavi Farr"
+          break;
+        case 15:
+          caption = "Photographer: Katie Chamberlain"
+          break;
+        case 16:
+          caption = "Photographer: Grégory Murphy"
+          break;
+        case 17:
+          caption = "Photographer: Audrey Dupupet"
+          break;
+        case 18:
+          caption = "Welcome to LHS Photography Club. Photographer: Divya Jain"
+          break;
+        default:
+          caption = ""
+          break;
+      }
+      $(".lb-wrapper").append("<a href='img/index/" + i + ".jpg' data-lightbox='indexPhotos' data-title='" + caption + "'><img src='img/index/" + i + ".jpg'></a>")
+    }
+  })
 })
